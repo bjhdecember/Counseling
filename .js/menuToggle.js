a013f1364ed8e6086575618f763e95a0041c7ca3
bjhdecember <barbarajh1202@yahoo.com>
2021-11-28 10:54:29 -0500

    add index.js

--- .js/menuToggle.js
+++ .js/menuToggle.js
@@ -1,5 +1,12 @@
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-}
-let x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu;
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classlist.toggle("active");
+    navMenu.classlist.toggle("active");
+})
+document.querySelectorAll(".menuItem").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
