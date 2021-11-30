927ffaf04eb9e4ffcab563db74c3e7e157338923
bjhdecember <barbarajh1202@yahoo.com>
2021-11-30 17:54:15 -0500

    changes

--- .js/menuToggle.js
+++ .js/menuToggle.js
@@ -1,11 +1,26 @@
+function validateEmail(value) {
+    var input = document.getElementsByTabIndex("2").value
+
+    var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+.)([a-z]+)(.[a-z])?$/
+
+    if (regx.tabIndex("mail")){
+        alert("You have provided a valid Email ID")
+        return true
+    }
+    else{
+        alert("Please provide a valid Email ID")
+        return false
+    }
+}
+
 const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    nav.classList.toggle("active");
 }
 
 const navLink = document.querySelectorAll(".nav-link");
@@ -14,5 +29,5 @@ navLink.forEach(n => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
     hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+    nav.classList.remove("active");
 }
