33280aa1de7c8c0032b76c90b33337781c8329ce
bjhdecember <barbarajh1202@yahoo.com>
2021-12-02 22:05:30 -0500

    changes

--- .js/menuToggle.js
+++ .js/menuToggle.js
@@ -1,33 +1,11 @@
-function validateEmail(value) {
-    var input = document.getElementsByTabIndex("2").value
-
-    var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9]+.)([a-z]+)(.[a-z])?$/
-
-    if (regx.tabIndex("mail")){
-        alert("You have provided a valid Email ID")
-        return true
-    }
-    else{
-        alert("Please provide a valid Email ID")
-        return false
-    }
-}
-
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".nav");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    nav.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".nav-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    nav.classList.remove("active");
-}
+var menuToggle = document.querySelector("#menu-toggle");
+var activeElements = document.querySelectorAll(".active-element");
+var toggledMenu = menuToggle.addEventListener("click", function(){
+     // forEach is not supported in IE11
+  // activeElements.forEach(function(e){
+  //     e.classList.toggle("active");
+  // });
+     for(var activated = 0; activated < activeElements.length; activated++){
+          activeElements[activated].classList.toggle("active");
+     }
+})
